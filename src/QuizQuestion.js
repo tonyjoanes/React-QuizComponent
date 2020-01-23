72b54194f8bb1668e777062c14948ad2e9570974
Tony Joanes <tonyjoanes@outlook.com>
2020-01-23 21:14:25 +0000

    answering quiz questions

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -15,13 +15,21 @@ class QuizQuestion extends Component {
                 <ul>
                 {
                   this.props.quiz_question.answer_options.map((answer_option, index) => {
-                    return <QuizQuestionButton key={index} button_text={answer_option} />
+                    return <QuizQuestionButton key={index} 
+                                               button_text={answer_option}
+                                               clickHandler={this.handleClick.bind(this)} />
                   })
                 }
                 </ul>
               </section>
             </main>
         )
+    }
+
+    handleClick(buttonText) {
+      if (buttonText === this.props.quiz_question.answer) {
+        this.props.showNextQuestionHandler();
+      }
     }
 }
 
--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -2,19 +2,19 @@ import React, { Component } from 'react';
 
 class QuizQuestionButton extends Component {
 
-    render() {
-        return (
-            <li>
-                <button onClick={this.handleClick.bind(this)}>
-                    {this.props.button_text}
-                </button>
-            </li>
-        )
-    }
+  render() {
+    return (
+      <li>
+        <button onClick={this.handleClick.bind(this)}>
+          {this.props.button_text}
+        </button>
+      </li>
+    )
+  }
 
-    handleClick() {
-
-    }
+  handleClick() {
+    this.props.clickHandler(this.props.button_text);
+  }
 }
 
 export default QuizQuestionButton;
