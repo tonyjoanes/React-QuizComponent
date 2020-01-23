d025a2bd530a65a30da0e5724bedd6cc50e22737
Tony Joanes <tonyjoanes@outlook.com>
2020-01-23 20:55:39 +0000

    Add quiz end code

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import QuizQuestion from './QuizQuestion';
+import QuizEnd from './QuizEnd';
 
 let quizData = require('./quiz_data.json');
 
-class Quiz extends Component {
-
+class Quiz extends Component { 
   constructor(props) {
     super(props);
     this.state = { quiz_position: 1};
   }
 
   render() {
+    const isQuizEnd = ((this.state.quiz_position -1) === quizData.quiz_questions.length)
+
     return (
-      <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]} />
+      <div>
+        {isQuizEnd 
+          ? <QuizEnd />
+          : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]} />
+        }
+      </div>
     )
   }
 }
